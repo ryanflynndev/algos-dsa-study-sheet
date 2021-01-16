5c7a13c2b48db4b6038f08f2c031fd47b27f25db
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-16 18:27:13 -0500

    Added selection sort

--- sorting-algos.js
+++ sorting-algos.js
@@ -17,4 +17,24 @@ function bubbleSort(arr){
 }
 //The reason for the variable "noSwaps" is so if you have a nearly sorted array it doesn't keep swapping
 
+//It is O(n**2), but it can be O(n) if the list is nearly sorted
+
 console.log(bubbleSort([45, 32, 87, 43]))
+
+let selectionSort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
+}
