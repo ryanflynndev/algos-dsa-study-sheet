a75a23ff58d8b9396ed6e32834cd59ecfcb7b9b7
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-19 17:43:16 -0500

    Added insertion sort with while loop

--- sorting-algos.js
+++ sorting-algos.js
@@ -40,16 +40,26 @@ function selectionSort(arr){
 
 console.log(selectionSort([34,22,10,19,17]))
 
-let insertionSort = (inputArr) => {
-    let length = inputArr.length;
-    for (let i = 1; i < length; i++) {
-        let key = inputArr[i];
-        let j = i - 1;
-        while (j >= 0 && inputArr[j] > key) {
-            inputArr[j + 1] = inputArr[j];
-            j = j - 1;
+
+//Worst case O(n**2) best case O(n). Best case is when the list is nearly sorted. The worst case would be something like [4,3,2,1]. Insertion sort would be a good online algorithm because if we insert items to an already sorted array it's easy to put them in their place.
+
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let currentVal = arr[i];
+        let j = i - 1
+        while(j >= 0 && arr[j] > currentVal) {
+            arr[j+1] = arr[j]
+            j--
         }
-        inputArr[j + 1] = key;
+        arr[j+1] = currentVal;
     }
-    return inputArr;
-};
+    return arr;
+}
+
+console.log(insertionSort([8,2,4,1,3]))
+
+//Insertion Sort Best: O(n), Average: O(n**2), Worst: O(n**2), Space Complexity: O(1)
+
+//Bubble Sort Best: O(n), Average: O(n**2), Worst: O(n**2), Space Complexity: O(1)
+
+//Selection Sort Best: O(n**2), Average: O(n**2), Worst: O(n**2), Space Complexity: O(1)
