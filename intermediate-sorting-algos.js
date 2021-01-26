acd7d5dbb168eb11b11bc0e4327eaf0161b3cc1a
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-26 14:50:38 -0500

    Added pivot for quick sort

--- intermediate-sorting-algos.js
+++ intermediate-sorting-algos.js
@@ -1,3 +1,5 @@
+//Merge Sort
+
 function merge(arr1, arr2){
     let result = []
     let i = 0
@@ -35,3 +37,30 @@ function mergeSort(arr){
 }
 
 console.log(mergeSort([2,3,8,9,2,4]))
+
+//Merge Sort is O(n log n) 
+//This is beceause we have O(log n) compositons and O(n) comparisons per decomposition
+
+//Pivot function for Quick Sort
+
+function swap(array, i , j){
+    let temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+}
+
+function pivot(arr, start=0, end=arr.length){
+    let pivot = arr[start]
+    let swapIndex = start
+    
+    for(let i = start + 1; i < end; i++){
+        if(pivot > arr[i]){
+            swapIndex++
+            swap(arr, swapIndex, i)
+        }
+    }
+    swap(arr, start, swapIndex)
+    return swapIndex
+}
+
+//Pivot returns the index, but it also swap all of the less than items to the left side of the existing array
