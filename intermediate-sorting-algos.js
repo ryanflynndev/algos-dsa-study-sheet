c2a63e1109e815589d3458b7a63ae88452f3e56a
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-29 21:54:06 -0500

    Added Radix

--- intermediate-sorting-algos.js
+++ intermediate-sorting-algos.js
@@ -64,3 +64,43 @@ function pivot(arr, start=0, end=arr.length){
 }
 
 //Pivot returns the index, but it also swap all of the less than items to the left side of the existing array
+
+
+//Radix Sort
+
+const getNum = (num, index) => {
+    const strNum = String(num);
+    let end = strNum.length - 1;
+    const foundNum = strNum[end - index];
+  
+    if (foundNum === undefined) return 0;
+    else return foundNum;
+  };
+
+  const largestNum = arr => {
+    let largest = "0";
+  
+    arr.forEach(num => {
+      const strNum = String(num);
+  
+      if (strNum.length > largest.length) largest = strNum;
+    });
+  
+    return largest.length;
+  };
+
+  const radixSort = arr => {
+    let maxLength = largestNum(arr);
+  
+    for (let i = 0; i < maxLength; i++) {
+      let buckets = Array.from({ length: 10 }, () => []);
+  
+      for (let j = 0; j < arr.length; j++) {
+        let num = getNum(arr[j], i);
+  
+        if (num !== undefined) buckets[num].push(arr[j]);
+      };
+      arr = buckets.flat();
+    };
+    return arr;
+  };
