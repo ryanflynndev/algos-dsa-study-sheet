eb3b60cb0db5200eafba5e11bf147e093e8a0b94
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-03 21:35:48 -0500

    Added addEdge method

--- graph.js
+++ graph.js
@@ -25,3 +25,14 @@ addVertex(v)
     // null array 
     this.AdjList.set(v, []); 
 } 
+
+addEdge(v, w) 
+{ 
+    // get the list for vertex v and put the 
+    // vertex w denoting edge between v and w 
+    this.AdjList.get(v).push(w); 
+  
+    // Since graph is undirected, 
+    // add an edge from w to v also 
+    this.AdjList.get(w).push(v); 
+} 
