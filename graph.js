69b85bc5a597e7d54f2a48d09f68667e7dcae4ad
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-05 20:22:47 -0500

    Added print graph method

--- graph.js
+++ graph.js
@@ -36,3 +36,27 @@ addEdge(v, w)
     // add an edge from w to v also 
     this.AdjList.get(w).push(v); 
 } 
+
+// Prints the vertex and adjacency list 
+printGraph() 
+{ 
+    // get all the vertices 
+    var get_keys = this.AdjList.keys(); 
+  
+    // iterate over the vertices 
+    for (var i of get_keys)  
+{ 
+        // great the corresponding adjacency list 
+        // for the vertex 
+        var get_values = this.AdjList.get(i); 
+        var conc = ""; 
+  
+        // iterate over the adjacency list 
+        // concatenate the values into a string 
+        for (var j of get_values) 
+            conc += j + " "; 
+  
+        // print the vertex and its adjacency list 
+        console.log(i + " -> " + conc); 
+    } 
+} 
